2d3a7d1d6f2b812d1e47c6c61ed670ca384eb76f
Vinicius Picolo <picolo.vininicius@gmail.com>
2023-08-01 02:11:51 -0300

    First migration

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -1,5 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using SalesWebMvc.Data;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<SalesWebMvcContext>(options =>
