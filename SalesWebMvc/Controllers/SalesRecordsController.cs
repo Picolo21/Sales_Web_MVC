a336cef1fd9037ca84624ac05cdf42e5eb94f2a7
Vinicius Picolo <picolo.vininicius@gmail.com>
2023-08-08 01:48:07 -0300

    Implementing grouping search

--- SalesWebMvc/Controllers/SalesRecordsController.cs
+++ SalesWebMvc/Controllers/SalesRecordsController.cs
@@ -31,9 +31,19 @@ namespace SalesWebMvc.Controllers
 			return View(result);
 		}
 
-		public IActionResult GroupingSearch()
-		{
-			return View();
-		}
-	}
+        public async Task<IActionResult> GroupingSearch(DateTime? initialDate, DateTime? finalDate)
+        {
+            if (!initialDate.HasValue)
+                initialDate = new DateTime(DateTime.Now.Year, 1, 1);
+
+            if (!finalDate.HasValue)
+                finalDate = DateTime.Now;
+
+            ViewData["initialDate"] = initialDate.Value.ToString("yyyy/MM/dd");
+            ViewData["finalDate"] = finalDate.Value.ToString("yyyy/MM/dd");
+
+            var result = await _salesRecordService.FindByDateGroupingAsync(initialDate, finalDate);
+            return View(result);
+        }
+    }
 }
--- SalesWebMvc/Services/SalesRecordService.cs
+++ SalesWebMvc/Services/SalesRecordService.cs
@@ -29,5 +29,25 @@ namespace SalesWebMvc.Services
                 .OrderByDescending(x => x.Date)
                 .ToListAsync();
         }
+
+        public async Task<List<IGrouping<Department,SalesRecord>>> FindByDateGroupingAsync(
+            DateTime? initialDate,
+            DateTime? finalDate)
+        {
+            var result = from salesRecord in _context.SalesRecord select salesRecord;
+
+            if (initialDate.HasValue)
+                result = result.Where(x => x.Date >= initialDate.Value);
+
+            if (finalDate.HasValue)
+                result = result.Where(x => x.Date <= finalDate.Value);
+
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .GroupBy(x => x.Seller.Department)
+                .ToListAsync();
+        }
     }
 }
